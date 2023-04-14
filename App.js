c4daed17341fe1a495a7df9479a9a33918ac8ba2
MIbrahim9276 <m.ibrahim9276@gmail.com>
2023-04-14 03:25:51 +0200

    Initialize React Native

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+      <Text>Test</Text>
+    </View>
+  );
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Fasten
-A Mobile App able to Help You with Your Fasting
