3a3db60db3d053e633c807c8315bfc08ba3d620f
MIbrahim9276 <m.ibrahim9276@gmail.com>
2023-04-20 16:42:49 +0200

    Reverted to React Native Paper

--- App.js
+++ App.js
@@ -6,29 +6,12 @@ This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-import {View, ScrollView, Text} from 'react-native';
-
-import css from './static/styles';
-import TimerScreen from './components/timer_screen';
-import GraphScreen from './components/graph_screen';
+import {View, Text} from 'react-native';
+import {Provider as PaperProvider, useTheme} from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={{height: "100%", width: "100%"}}>
-      <View style={css.topBar}>
-        <Text style={css.title}>Fasten</Text>
-      </View>
-      
-      <ScrollView
-        style={css.screen}
-        horizontal={true}
-        pagingEnabled={true}
-        showsHorizontalScrollIndicator={false}>
-
-        <TimerScreen />
-        <GraphScreen />
-
-      </ScrollView>
-    </View>
+    <PaperProvider>
+    </PaperProvider>
   );
 }
